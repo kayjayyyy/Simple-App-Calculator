18a9879fdf96799e6839be5d6ee0ba8355e5e0d9
unknown <templanzatintin@gmail.com>
2023-05-12 13:26:36 +0800

    greetings

---
+++ SimpleCalculator.py
@@ -0,0 +1,9 @@
+# Templanza, Kristine Joy F.
+# BSCPE 1-4
+# Asignnment no. 5 - Simple App Calculator
+
+# Greeting and border line
+print("")
+print("\033[35m※ \033[0m" * 40)
+print("")
+print("\033[45m ♥ Welcome to our calculator program! ♥ \033[0m".center(85))
