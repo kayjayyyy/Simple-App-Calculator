d79b216fc062d930057bcb87c50e7d1f0a01571b
unknown <templanzatintin@gmail.com>
2023-05-12 18:10:30 +0800

    ask for the users name

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -8,13 +8,20 @@ print("\033[35m※ \033[0m" * 40)
 print("")
 print("\033[45m ♥ Welcome to our calculator program! ♥ \033[0m".center(85))
 
+# Ask the name of the user 
+name = input("\n\033[033mGood day! What is your name? \033[0m")
+print("\n\033[3;33mI hope you are doing well,", name + "!\033[0m")
+print("")
+
+print("\033[36m Let's get started! \033[0m".center(78, "~"))
+
 print("\n")
 print("\033[36ma. Multiplication\033[0m".center(85))
 print("\033[36mb. Division\033[0m".center(85))
 print("\033[36mc. Addition\033[0m".center(85))
 print("\033[36md. Subtraction\033[0m".center(85))
 
-math = input("\nPlease choose a math operation you want to use (a/b/c/d): ")
+math = input("\n\033[33;3mPlease choose a math operation you want to use (a/b/c/d): \033[0m")
 
-first_number = float(input("\nEnter your first number: "))
-second_number = float(input("\nEnter your second number: "))
+first_number = float(input("\n\033[33;3mEnter your first number: \033[0m"))
+second_number = float(input("\n\033[33;3mEnter your second number: \033[0m"))
